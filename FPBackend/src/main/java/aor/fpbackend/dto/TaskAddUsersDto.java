6507f8b69a8d14216cb028a5690e1fbca6fd3f88
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-06-05 16:10:09 +0100

    Update bi-directional relations

--- FPBackend/src/main/java/aor/fpbackend/dto/TaskAddUsersDto.java
+++ FPBackend/src/main/java/aor/fpbackend/dto/TaskAddUsersDto.java
@@ -1,6 +1,7 @@
 package aor.fpbackend.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import jakarta.xml.bind.annotation.XmlElement;
@@ -15,6 +16,7 @@ public class TaskAddUsersDto implements Serializable {
 
 
     @XmlElement
+    @NotNull
     private long taskId;
 
     @XmlElement
