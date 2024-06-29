ae6268c0c1aef2f9b93000550b4e56b9bee7e0b7
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-06-29 14:01:26 +0100

    Project Report

--- FPBackend/src/main/java/aor/fpbackend/enums/LocationEnum.java
+++ FPBackend/src/main/java/aor/fpbackend/enums/LocationEnum.java
@@ -1,10 +1,21 @@
 package aor.fpbackend.enums;
 
 public enum LocationEnum {
-    LISBOA,
-    COIMBRA,
-    PORTO,
-    TOMAR,
-    VISEU,
-    VILA_REAL
+    LISBOA("Lisboa"),
+    COIMBRA("Coimbra"),
+    PORTO("Porto"),
+    TOMAR("Tomar"),
+    VISEU("Viseu"),
+    VILA_REAL("Vila Real");
+
+    private final String value;
+
+    LocationEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
