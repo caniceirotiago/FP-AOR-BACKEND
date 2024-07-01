7722ee31875cdd32df594b45979c3e4e6521249f
caniceiro <caniceiro@dei.uc.pt>
2024-07-01 12:21:16 +0100

    Alterações dto Projetcs per user

--- FPBackend/src/main/java/aor/fpbackend/dto/Project/ProjectNameIdDto.java
+++ FPBackend/src/main/java/aor/fpbackend/dto/Project/ProjectNameIdDto.java
@@ -1,15 +1,23 @@
 package aor.fpbackend.dto.Project;
 
+import aor.fpbackend.enums.ProjectStateEnum;
+
+import java.time.Instant;
+
 public class ProjectNameIdDto {
     private long id;
     private String name;
+    private Instant createdAt;
+    private Enum<ProjectStateEnum> status;
 
     public ProjectNameIdDto() {
     }
 
-    public ProjectNameIdDto(long id, String name) {
+    public ProjectNameIdDto(long id, String name, Instant createdAt, Enum<ProjectStateEnum> status) {
         this.id = id;
         this.name = name;
+        this.createdAt = createdAt;
+        this.status = status;
     }
 
     public long getId() {
@@ -26,5 +34,21 @@ public class ProjectNameIdDto {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Enum<ProjectStateEnum> getStatus() {
+        return status;
+    }
+
+    public void setStatus(Enum<ProjectStateEnum> status) {
+        this.status = status;
     }
 }
