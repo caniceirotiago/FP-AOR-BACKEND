e86544201f70eac9053ded694834fca778dde509
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-07-02 13:15:57 +0100

    melhorias dia 02.07

--- FPBackend/src/main/java/aor/fpbackend/bean/MembershipBean.java
+++ FPBackend/src/main/java/aor/fpbackend/bean/MembershipBean.java
@@ -47,11 +47,11 @@ public class MembershipBean implements Serializable {
     /**
      * Handles the request of a user to join a project.
      *
-     * @param projectId the ID of the project to join
+     * @param projectId       the ID of the project to join
      * @param securityContext the security context containing the authenticated user
-     * @throws EntityNotFoundException if the user or project is not found
+     * @throws EntityNotFoundException      if the user or project is not found
      * @throws DuplicatedAttributeException if the user is already a member of the project
-     * @throws UnknownHostException if an unknown host exception occurs
+     * @throws UnknownHostException         if an unknown host exception occurs
      */
     public void askToJoinProject(long projectId, SecurityContext securityContext) throws EntityNotFoundException, DuplicatedAttributeException, UnknownHostException {
         AuthUserDto authUserDto = (AuthUserDto) securityContext.getUserPrincipal();
@@ -96,8 +96,8 @@ public class MembershipBean implements Serializable {
      * Sends an email requisition to all project managers for a user's request to join a project.
      *
      * @param membershipEntity the project membership entity containing the join request details
-     * @param user the user requesting to join the project
-     * @param projectEntity the project to which the user is requesting to join
+     * @param user             the user requesting to join the project
+     * @param projectEntity    the project to which the user is requesting to join
      */
     public void sendJoinRequisitionToManagers(ProjectMembershipEntity membershipEntity, UserEntity user, ProjectEntity projectEntity) {
         List<UserEntity> projectManagers = projectMemberDao.findProjectManagers(projectEntity.getId());
@@ -110,13 +110,13 @@ public class MembershipBean implements Serializable {
     /**
      * Confirms or rejects a user's request to join a project based on a provided token.
      *
-     * @param token the token associated with the join request
-     * @param approve true if the request is approved, false if rejected
+     * @param token            the token associated with the join request
+     * @param approve          true if the request is approved, false if rejected
      * @param approverUsername the username of the approver
-     * @throws EntityNotFoundException if the project membership or approver user is not found
-     * @throws UserNotFoundException if the approver user is not found
+     * @throws EntityNotFoundException     if the project membership or approver user is not found
+     * @throws UserNotFoundException       if the approver user is not found
      * @throws UnauthorizedAccessException if the approver is not a project manager
-     * @throws UnknownHostException if an error occurs during notification creation
+     * @throws UnknownHostException        if an error occurs during notification creation
      */
     public void confirmAskToJoinProjectInvite(String token, boolean approve, String approverUsername) throws EntityNotFoundException, UserNotFoundException, UnauthorizedAccessException, UnknownHostException {
         ProjectMembershipEntity membershipEntity = projectMemberDao.findProjectMembershipByAcceptanceToken(token);
@@ -130,7 +130,7 @@ public class MembershipBean implements Serializable {
         if (!projectMemberDao.isUserProjectManager(membershipEntity.getProject().getId(), approverEntity.getId())) {
             throw new UnauthorizedAccessException("Approver is not a Project Manager");
         }
-        try{
+        try {
             if (approve) {
                 membershipEntity.setAccepted(true);
                 membershipEntity.setAcceptanceToken(null);
@@ -158,8 +158,8 @@ public class MembershipBean implements Serializable {
      * Sends an invitation to a user to join a project and creates a notification for the invite.
      *
      * @param membershipEntity the project membership entity containing invitation details
-     * @param user the user to be invited
-     * @param projectEntity the project to which the user is invited
+     * @param user             the user to be invited
+     * @param projectEntity    the project to which the user is invited
      * @throws UnknownHostException if an error occurs during notification creation
      */
     public void sendInviteToUser(ProjectMembershipEntity membershipEntity, UserEntity user, ProjectEntity projectEntity) throws UnknownHostException {
@@ -180,15 +180,15 @@ public class MembershipBean implements Serializable {
     /**
      * Adds a user to a project, either directly or via an invite, and creates the necessary notifications and logs.
      *
-     * @param username the username of the user to be added
-     * @param projectId the ID of the project to which the user is being added
+     * @param username          the username of the user to be added
+     * @param projectId         the ID of the project to which the user is being added
      * @param createHasAccepted flag indicating if the user has already accepted the invite
-     * @param isTheCreator flag indicating if the user is the creator of the project
-     * @param authUser the authenticated user performing the action
-     * @throws EntityNotFoundException if the project or user is not found
-     * @throws UserNotFoundException if the user is not found
+     * @param isTheCreator      flag indicating if the user is the creator of the project
+     * @param authUser          the authenticated user performing the action
+     * @throws EntityNotFoundException  if the project or user is not found
+     * @throws UserNotFoundException    if the user is not found
      * @throws InputValidationException if there is a validation error
-     * @throws UnknownHostException if an error occurs during notification creation
+     * @throws UnknownHostException     if an error occurs during notification creation
      */
     @Transactional
     public void addUserToProject(String username, long projectId, boolean createHasAccepted, boolean isTheCreator, UserEntity authUser) throws EntityNotFoundException, UserNotFoundException, InputValidationException, UnknownHostException {
@@ -207,7 +207,7 @@ public class MembershipBean implements Serializable {
         if (projectDao.isProjectMember(projectId, userEntity.getId())) {
             throw new IllegalStateException("User is already a member of the project");
         }
-        try{
+        try {
             ProjectMembershipEntity membershipEntity = new ProjectMembershipEntity();
             membershipEntity.setUser(userEntity);
             membershipEntity.setProject(projectEntity);
@@ -243,51 +243,52 @@ public class MembershipBean implements Serializable {
     /**
      * Handles the acceptance or rejection of a project invite based on the provided token.
      *
-     * @param token the acceptance token associated with the project invite
+     * @param token   the acceptance token associated with the project invite
      * @param approve flag indicating whether the invite is approved or not
      * @throws EntityNotFoundException if the project membership is not found
-     * @throws UnknownHostException if an error occurs during notification creation
+     * @throws UnknownHostException    if an error occurs during notification creation
      */
     public void acceptProjectInvite(String token, boolean approve) throws EntityNotFoundException, UnknownHostException {
-       if (token == null) {
+        if (token == null) {
             throw new EntityNotFoundException("Project membership not found");
-       }
-       ProjectMembershipEntity membershipEntity = projectMemberDao.findProjectMembershipByAcceptanceToken(token);
-       if (membershipEntity == null) {
-           throw new EntityNotFoundException("Project membership not found");
-       }
-       try{
-           if (approve) {
+        }
+        ProjectMembershipEntity membershipEntity = projectMemberDao.findProjectMembershipByAcceptanceToken(token);
+        if (membershipEntity == null) {
+            throw new EntityNotFoundException("Project membership not found");
+        }
+        try {
+            if (approve) {
                 membershipEntity.setAccepted(true);
                 membershipEntity.setAcceptanceToken(null);
                 String content = "User " + membershipEntity.getUser().getUsername() + ", accepted become project member";
                 notificationBean.createNotificationForProjectManagersKnowUserApproval(membershipEntity, true);
                 projectBean.createProjectLog(membershipEntity.getProject(), membershipEntity.getUser(), LogTypeEnum.PROJECT_MEMBERS, content);
                 LOGGER.info("User " + membershipEntity.getUser().getUsername() + " accepted to become project member");
-           } else {
-               String content = "User " + membershipEntity.getUser().getUsername() + ", refused become project member";
-               notificationBean.createNotificationForProjectManagersKnowUserApproval(membershipEntity, false);
-               projectBean.createProjectLog(membershipEntity.getProject(), membershipEntity.getUser(), LogTypeEnum.PROJECT_MEMBERS, content);
-               projectMemberDao.remove(membershipEntity);
-               LOGGER.info("User " + membershipEntity.getUser().getUsername() + " refused to become project member");
-           }
-       } catch (Exception e) {
-           LOGGER.error("Error while accepting project invite", e);
-           throw e;
-       } finally {
-           ThreadContext.clearAll();
-       }
+            } else {
+                String content = "User " + membershipEntity.getUser().getUsername() + ", refused become project member";
+                notificationBean.createNotificationForProjectManagersKnowUserApproval(membershipEntity, false);
+                projectBean.createProjectLog(membershipEntity.getProject(), membershipEntity.getUser(), LogTypeEnum.PROJECT_MEMBERS, content);
+                projectMemberDao.remove(membershipEntity);
+                LOGGER.info("User " + membershipEntity.getUser().getUsername() + " refused to become project member");
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error while accepting project invite", e);
+            throw e;
+        } finally {
+            ThreadContext.clearAll();
+        }
     }
 
 
     /**
      * Removes a user from a project and creates appropriate notifications and logs.
+     * Reassign tasks the user is responsible for to the project creator.
      *
-     * @param username the username of the user to be removed
-     * @param projectId the ID of the project from which the user is to be removed
+     * @param username        the username of the user to be removed
+     * @param projectId       the ID of the project from which the user is to be removed
      * @param securityContext the security context containing the authenticated user's information
      * @throws EntityNotFoundException if the project or user is not found
-     * @throws UnknownHostException if an error occurs during notification creation
+     * @throws UnknownHostException    if an error occurs during notification creation
      */
     @Transactional
     public void removeUserFromProject(String username, long projectId, SecurityContext securityContext) throws EntityNotFoundException, UnknownHostException, ForbiddenAccessException {
@@ -302,14 +303,22 @@ public class MembershipBean implements Serializable {
         if (userEntity == null) {
             throw new EntityNotFoundException("User not found");
         }
-        if((!projectMemberDao.isUserProjectManager(projectId, authUserEntity.getId())) && (authUserEntity.getId()!=userEntity.getId())){
+        if ((!projectMemberDao.isUserProjectManager(projectId, authUserEntity.getId())) && (authUserEntity.getId() != userEntity.getId())) {
             throw new ForbiddenAccessException("Not allowed");
         }
         if (projectEntity.getCreatedBy().equals(userEntity)) {
             throw new IllegalStateException("User is the creator of the project");
         }
-        ProjectMembershipEntity userMembership = null;
+
+        // Check if the user is responsible for any tasks in the project
+        UserEntity projectCreator = projectEntity.getCreatedBy();
+        for (TaskEntity task : projectEntity.getTasks()) {
+            if (task.getResponsibleUser() != null && task.getResponsibleUser().equals(userEntity)) {
+                task.setResponsibleUser(projectCreator); // Reassign to project creator
+            }
+        }
         // Check if the user is a member of the project
+        ProjectMembershipEntity userMembership = null;
         for (ProjectMembershipEntity membership : projectEntity.getMembers()) {
             if (membership.getUser().equals(userEntity)) {
                 userMembership = membership;
@@ -348,7 +357,7 @@ public class MembershipBean implements Serializable {
      * Retrieves a list of users' basic information whose usernames start with a specified first letter and are members of a specified project.
      *
      * @param firstLetter the first letter to filter the usernames
-     * @param projectId the ID of the project
+     * @param projectId   the ID of the project
      * @return a list of UserBasicInfoDto containing the basic information of users
      */
     public List<UserBasicInfoDto> getUsersBasicInfoByFirstLetter(String firstLetter, long projectId) {
