e2502ad6b2fef82b818e7723f8128a8159410b52
caniceiro <caniceiro@dei.uc.pt>
2024-07-11 22:34:40 +0100

    correção varios bugs

--- FPBackend/src/main/java/aor/fpbackend/dto/User/UserRegisterDto.java
+++ FPBackend/src/main/java/aor/fpbackend/dto/User/UserRegisterDto.java
@@ -19,7 +19,7 @@ public class UserRegisterDto implements Serializable {
     @XmlElement
     @NotNull
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#.])[A-Za-z\\d@$!%*?&#.]+$",
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#.+])[A-Za-z\\d@$!%*?&#.+]+$",
             message = "Password must include an uppercase letter, a lowercase letter, a digit, and one special character")
     private String password;
 
