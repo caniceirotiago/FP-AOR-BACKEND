0c6a5533826f27aa0e1cdeebfd50626ab0b4b2d8
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-07-12 11:36:49 +0100

    default photo

--- FPBackend/src/main/java/aor/fpbackend/utils/GlobalSettings.java
+++ FPBackend/src/main/java/aor/fpbackend/utils/GlobalSettings.java
@@ -1,7 +1,7 @@
 package aor.fpbackend.utils;
 
 public class GlobalSettings {
-    public static final String USER_DEFAULT_PHOTO_URL = "https://www.silcharmunicipality.in/wp-content/uploads/2021/02/male-face.jpg";
+    public static final String USER_DEFAULT_PHOTO_URL = "https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659651_640.png";
     public static final int CONFIRMATION_TOKEN_EXPIRATION_TIME_H = 24;
     public static final int PASSWORD_RESET_PREVENT_TIMER_MIN = 30;
     public static final int CONFIRMATION_EMAIL_PREVENT_TIMER_MIN = 1;
